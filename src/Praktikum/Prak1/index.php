c6ef602b8f3f76fd5db8bf61f397c301ab9d7a9c
Soukaina Elhafif <soukainaelhafif@MacBook-Air-von-Soukaina.local>
2025-11-02 01:37:14 +0100

    HTML5-Validierung durchgeführt und Layout bereinigt

---
+++ src/Praktikum/Prak1/index.php
@@ -0,0 +1,15 @@
+<?php
+$title = "Übersicht";
+require_once("head.php");
+?>
+<main>
+  <h2>Willkommen beim Pizzaservice 🍕</h2>
+  <p>Wählen Sie eine Seite:</p>
+  <ul>
+    <li><a href="bestellung.php">Bestellung</a></li>
+    <li><a href="baecker.php">Bäcker</a></li>
+    <li><a href="fahrer.php">Fahrer</a></li>
+    <li><a href="kunde.php">Kunde</a></li>
+  </ul>
+</main>
+<?php require_once("footer.php"); ?>
